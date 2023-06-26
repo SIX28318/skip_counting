11ea9da3801fd8776b2b9924d9883dc974c79f52
SIX28318 <137732423+SIX28318@users.noreply.github.com>
2023-06-25 20:13:08 -0400

    Update skip_counting.rs
    Prettified presentation, swapping x with × (U+00D7)

--- skip_counting.rs
+++ skip_counting.rs
@@ -2,10 +2,10 @@
 // written in Rust.
 
 // Example:
-// 1 x 1 = 1
-// 1 x 2 = 2
+// 1 × 1 = 1
+// 1 × 2 = 2
 // ...
-// 20 x 20 = 400
+// 20 × 20 = 400
 
 fn main() {
     // Iterate over a range, here 1 through 20.
@@ -13,7 +13,7 @@ fn main() {
         println!(""); // Dirty line break. Prettify this later...
         let digit = n;
         for (_index, value) in (1..21).enumerate() {
-            println!("{} x {} = {} ", digit, value, digit*value);
+            println!("{} × {} = {} ", digit, value, digit*value);
         }
     }
 }
